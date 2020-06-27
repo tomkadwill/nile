467cd3a1f856285c67bddb54ee5cd4061b5c45c4
Tom Kadwill <tomkadwill@gmail.com>
2020-06-27 11:57:24 +0100

    pt 2

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get books_index_url
+    assert_response :success
+  end
+
+end
