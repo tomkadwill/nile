64dfb8b5c9c9edd7e6bbd040136f626efedbd169
Tom Kadwill <tomkadwill@gmail.com>
2020-07-04 16:51:46 +0100

    pt 3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,20 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unproccessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
 end
