d527a30ca5a55e2ba0fb9651fab3c4b3b388c36d
Tom Kadwill <tomkadwill@gmail.com>
2020-07-31 07:32:12 +0100

    video 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_destroyed(e)
+    render json: { errors: e.record.errors }, status: :unprocessable_entity
+  end
 end
